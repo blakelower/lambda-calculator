394c2c97e4735f32493c289f706162e250113f53
blakelower <blakelower1999@gmail.com>
2019-11-05 23:03:20 +0700

    tried adding some css

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -2,16 +2,17 @@ import React from "react";
 import SpecialButtons from "./SpecialButton"
 //import any components needed
 import { specials } from '../../../data';
+import './specials.css'
 //Import your array data to from the provided data file
 
 const Specials = () => {
     // STEP 2 - add the imported data to state
     const [special, Setspecial] = React.useState(specials)
     return (
-        <div>
+        <div className="special">
             {
                 special.map((element) => {
-                    return <button>{element.value}</button>
+                return <button>{element}</button>
                 })
             }
         </div>
